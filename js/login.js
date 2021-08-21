5677b44923d1d2aba610a8728edba4d9b59a7b4e
elianarm <armanelian@gmail.com>
2021-08-21 20:53:41 -0300

    Autenticacion de google 1

--- js/login.js
+++ js/login.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 
 });
+
+//Autenticacion de google
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+}
+
+
